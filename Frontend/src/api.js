d9f99618d30302d60d5f00afd12c277426a83dfb
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2023-10-29 01:45:30 -0300

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -4,11 +4,7 @@ class Api {
 
   // Configuração padrão do Axios
   api = axios.create({
-    // baseURL: 'https://api-login-0qlo.onrender.com',
     baseURL: 'http://localhost:3001',
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
   });
   
   // Exemplo de função para fazer uma solicitação de registro de usuário
